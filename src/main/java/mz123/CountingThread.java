b87d0fffc7140150c9d2fea729da3ba93cc0e660
Administrator <aa>
2018-08-23 13:30:09 +0200

    nowy plik

--- src/main/java/mz123/CountingThread.java
+++ src/main/java/mz123/CountingThread.java
@@ -1,4 +1,9 @@
 package mz123;
 
-public class CountingThread {
+public class CountingThread extends Thread{
+    public void run(){
+        for (int i = 0; i<10; i++){
+            System.out.println((Thread.currentThread().getName() + ": " + i));
+        }
+    }
 }
