d67ab8430adcab8fd717c97ab21be4758fc1f047
Administrator <aa>
2018-08-23 12:43:29 +0200

    Initial commit

---
+++ src/main/java/mz123/TestBazy.java
@@ -0,0 +1,29 @@
+package mz123;
+
+import java.sql.*;
+
+public class TestBazy {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Test");
+        try {
+            Connection con = DriverManager.getConnection("jdbc:h2:tcp://localhost/~/test", "sa", "sa");
+            Statement st = con.createStatement();
+            ResultSet res = st.executeQuery("select * from mztest");
+
+            while (res.next()) {
+                int id = res.getInt("ID");
+                String name = res.getString("NAME");
+                String desc = res.getString("DESC");
+                System.out.println("" + id + " " + name + " " + desc);
+            }
+            st.close();
+            con.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
